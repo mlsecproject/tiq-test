4a1cf2a03e8015427aa56e28c69dc0862a479be6
Alexandre Pinto <alexcp@gmail.com>
2014-07-25 11:55:37 -0700

    Implemented loading of a TI resource from the 'database'

--- tiq-test.R
+++ tiq-test.R
@@ -2,17 +2,24 @@
 ##
 ##
 
-source("utils/log.config.R")
+source("utils/log-config.R")
 source("utils/tiq-data.R")
 
 #####
 ## CONFIGURATION
 ##
-enrich.group = "public_inbound"
+group = "public_inbound"
 start.date = as.Date("20140615", format="%Y%M%d")
 end.date = as.Date("20140715", format="%Y%M%d")
 
+category = "raw"
+
 # tiq.test.noveltyTest
-tiq.test.noveltyTest <- function(enrich.group, start.date, end.date) {
+tiq.test.noveltyTest <- function(group, start.date, end.date) {
+
+  ## Getting the raw data group path
+
+  ## For each date
+
 
 }
