208cb28fd0916c95f1ac08556fd7dfbcedaf7365
Alexandre Pinto <alexcp@gmail.com>
2014-08-02 13:16:21 -0700

    Added documentation for tiq-data and tiq-helper (#6)

--- utils/tiq-data.R
+++ utils/tiq-data.R
@@ -5,19 +5,21 @@
 ## This module attemps to abstract the loading of TI feeds using the directory
 ## structure we propose for the initial release.
 
-## Requires: Usage of `data.table` and `testthat` packages
+# Requires: Usage of `data.table` and `testthat` packages
 library(data.table)
 library(testthat)
 
-## The root path is the local working directory of tiq-test scripts with an added
-## "data" path. Change here if you want it to point to a different place.
+################################################################################
+## Helper path bulding and enumeration functions for the 'database'
+################################################################################
+# The root path is the local working directory of tiq-test scripts with an added
+# "data" path. Change here if you want it to point to a different place.
 .tiq.data.rootPath = file.path(getwd(), "data")
-.tiq.data.defaultTIFields = c("entity", "type", "direction", "source", "notes", "date")
 
-## tiq.data.getDirPath
-## Builds the path for the intel analysts sources.
-##  - category: currently one of "raw", "enriched", "population"
-##  - group: the intel group you want the path for
+# tiq.data.getDirPath - returns 'character'
+# Builds the path for the intel analysts sources and returns it.
+#  - category: currently one of "raw", "enriched", "population"
+#  - group: the intel group you want the path for
 .tiq.data.getDirPath <- function(category, group) {
   path = file.path(.tiq.data.rootPath, category, group)
   if (!file.exists(path)) {
@@ -31,6 +33,12 @@ library(testthat)
   return(path)
 }
 
+# tiq.data.getAvailableDates - returns 'character' vector
+# Queries the data repository for the specific 'category' and 'group' of TI
+# or population information and return the dates we have available for that
+# group.
+#  - category: currently one of "raw", "enriched", "population"
+#  - group: the intel group you want the path for
 .tiq.data.getAvailableDates <- function(category, group) {
   path = .tiq.data.getDirPath(category, group)
 
@@ -42,6 +50,29 @@ library(testthat)
   return(dates)
 }
 
+################################################################################
+## TI and Population loading data functions
+################################################################################
+# tiq.data.loadTI - returns a 'data.table' with at least '.tiq.data.defaultTIFields'
+# Fetches the data contained in the Threat Intelligence dataset contained in
+# 'category' and 'group' on the 'date' specified.
+#  - category: currently one of "raw" or "enriched"
+#  - group: the intel group you want the path for
+#  - date: a date string in the format "YYYYMMDD", or NULL if you want the
+#          date the 'database' has available
+.tiq.data.defaultTIFields = c("entity", "type", "direction", "source", "notes", "date")
+tiq.data.loadTI <- function(category, group, date=NULL) {
+  return(.tiq.data.loadData(category, group, date, valid.fields=.tiq.data.defaultTIFields))
+}
+
+# tiq.data.loadPopulation - returns a 'data.table' with 'pop.id'and 'totalIPs'
+# Fetches the data contained in the Population dataset contained in
+# 'pop.group', with the 'pop.id' as the key on the 'date' specified.
+#  - pop.group: the intel group you want the path for
+#  - pop.id: the key of the population dataset. If not present in the dataset
+#            the function will error out.
+#  - date: a date string in the format "YYYYMMDD", or NULL if you want the
+#          date the 'database' has available
 tiq.data.loadPopulation <- function(pop.group, pop.id, date = NULL) {
   pop.data = .tiq.data.loadData("population", pop.group, date, valid.fields=pop.id)
   pop.data[, totalIPs := as.numeric(totalIPs)]
@@ -51,10 +82,15 @@ tiq.data.loadPopulation <- function(pop.group, pop.id, date = NULL) {
   return(pop.data)
 }
 
-tiq.data.loadTI <- function(category, group, date=NULL) {
-  return(.tiq.data.loadData(category, group, date, valid.fields=.tiq.data.defaultTIFields))
-}
-
+# .tiq.data.loadData - returns a 'data.table'
+# Lower level function helper for loadTI and loadPopulation. Should not be used
+# directly
+#  - category: currently one of "raw", "enriched" or "population"
+#  - group: the intel group you want the path for
+#  - date: a date string in the format "YYYYMMDD", or NULL if you want the
+#          date the 'database' has available
+#  - valid.fields: the fields we should look for in the dataset to validate that
+#                  it is of the correct type
 .tiq.data.loadData <- function(category, group, date=NULL, valid.fields=character(0)) {
 
   test_that("tiq.data.loadData: unable to open data - 'category' or 'group' are invalid", {
